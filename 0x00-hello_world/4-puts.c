1e3c252e264a0982f37060bd2124e5cb63aea35c
MayarSamy9 <mayarsamy24122501@gmail.com>
2023-12-14 10:50:02 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: a C program that prints usings puts function
+ *
+ * Return: always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
